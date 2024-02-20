f93ba547b98daaf9ffa90d9aa76cd45b4b2a6aab
Mahmoud Hamed <mahmoud.hamed@blink22.com>
2024-02-20 12:55:14 +0200

    fix: edit msr and var endpoints routes

--- index.ts
+++ index.ts
@@ -24,11 +24,11 @@ app.post('/payoutsSummary', async (req: Request, res: Response) => {
     res.json(await generatePayoutsSummaryResponse(req.body as PayoutsSummaryRequest));
 })
 
-app.post('/tax-invoices', async (req: Request, res: Response) => {
+app.post('/getVatSummary', async (req: Request, res: Response) => {
     res.json(await getTaxInvoices(req.body));
 });
 
-app.post("/monthly-statement-reports", async (req: Request, res: Response) => {
+app.post("/getMsrSummary", async (req: Request, res: Response) => {
     res.json(await getMonthlyStatementReports(req.body));
 })
 app.get('/tax-available-years/:merchantId', (req: Request, res: Response) => {
